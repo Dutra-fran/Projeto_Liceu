f907e86f349465c0ec7d290fd9af6d34a79c6a14
Dutra-fran <63562657+Dutra-fran@users.noreply.github.com>
2020-10-31 22:07:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-		<head>
-				<title>Site de testes</title>
-				<meta charset="UTF-8" />
-		</head>
-		<body>
-				<h1>Testes</h1>
-				<p>Olá! Para testar os demais recursos disponíveis no site para teste é necessário fazer um registro e logo após efetuar o login.</p>
+   <head>
+      <title>Site de testes</title>
+      <meta charset="UTF-8" />
+   </head>
+   <body>
+      <h1>Testes</h1>
+      <p>Olá! Para testar os demais recursos disponíveis no site para teste é necessário fazer um registro e logo após efetuar o login.</p>
 		
-				<ul>
-						<li><a href="./login/login.php">Login</a></li>
-						<li><a href="./registro/registrar.php">Registrar</a></li>
-				</ul>
+      <ul>
+         <li><a href="./login/login.php">Login</a></li>
+         <li><a href="./registro/registrar.php">Registrar</a></li>
+      </ul>
 		
-				<?php
-						session_start();
+      <?php
+         session_start();
 				
-						if(isset($_SESSION['mensagem_painel'])){
-         echo "<h2>".$_SESSION['mensagem_painel']."</h2><br><br>";
-         unset($_SESSION['mensagem_painel']);
-       }
-				
-		  		if(isset($_SESSION['mensagem_admin'])){
-          echo "<h2>".$_SESSION['mensagem_admin']."</h2><br><br>";
-          unset($_SESSION['mensagem_admin']);
-       }
-     ?>
-		</body>
+         if(isset($_SESSION['mensagem_painel'])){
+            echo "<h2>".$_SESSION['mensagem_painel']."</h2><br><br>";
+            unset($_SESSION['mensagem_painel']);
+         }
+      ?>
+   </body>
 </html>
