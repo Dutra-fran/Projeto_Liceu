6cfd852a7c1e398c3bba26c11cdd88749bcb44a2
Dutra-fran <63562657+Dutra-fran@users.noreply.github.com>
2020-11-01 21:38:11 -0300

    Update processa.php

--- login/processa.php
+++ login/processa.php
@@ -23,36 +23,10 @@
     $_SESSION['id'] = $id;
     $_SESSION['senha'] = $dados['Senha'];
     $_SESSION['aluno'] = "TRUE";
-    $query3 = "DELETE FROM Status WHERE ID_Cadastro = '".$_SESSION['id']."'";
-    $result3 = mysqli_query($conn, $query3);
-    $query2 = "INSERT INTO Status (Status, ID_Cadastro) VALUES ('Online', '".$dados['ID']."')";
-    $result2 = mysqli_query($conn, $query2);
-
-    if(isset($_SERVER['HTTP_USER_AGENT'])){
-
-      $query6 = "SELECT * FROM Dados WHERE ID_Cadastro = '".$_SESSION['id']."'";
-      $result6 = mysqli_query($conn, $query6);
-      $row6 = mysqli_num_rows($result6);
-
-      if($row6 == 1){
-        $query7 = "DELETE FROM Dados WHERE ID_Cadastro = '".$_SESSION['id']."'";
-        $result7 = mysqli_query($conn, $query7);
-      }
-
-      if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
-        $query4 = "INSERT INTO Dados (Dados, IP, ID_Cadastro) VALUES ('".$_SERVER['HTTP_USER_AGENT']."', '".$_SERVER['HTTP_X_FORWARDED_FOR']."', '".$_SESSION['id']."')";
-        $result4 = mysqli_query($conn, $query4);
-      } elseif(isset($_SERVER['REMOTE_ADDR'])){
-        $query5 = "INSERT INTO Dados (Dados, IP, ID_Cadastro) VALUES ('".$_SERVER['HTTP_USER_AGENT']."', '".$_SERVER['HTTP_X_FORWARDED_FOR']."', '".$_SESSION['id']."')";
-        $result5 = mysqli_query($conn, $query5);
-      }
-    }
+    header('Location: ../painel/painel.php');
+    exit();
   } else {
     $_SESSION['mensagem_login'] = "Usuário ou senha incorretos!";
     header('Location: ./login.php');
     exit();
   }
-
-
-  header('Location: ../painel/painel.php');
-  exit();
