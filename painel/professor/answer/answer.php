6f3a109364dfddf1adb47c19bac403975110b690
Dutra-fran <63562657+Dutra-fran@users.noreply.github.com>
2020-11-17 23:07:13 -0300

    Update answer.php
    Arrumando a codificação do arquivo.

--- painel/professor/answer/answer.php
+++ painel/professor/answer/answer.php
@@ -10,8 +10,8 @@
             include_once('../../../conexao.php');
             include_once('./verifica_login.php');
             
-            $query = "SELECT * FROM Salas_Prof WHERE ID_Professor = '".$_SESSION['id']."'";
-            $result = mysqli_query($conn, $query);
+            $Salas_Prof_query = "SELECT * FROM Salas_Prof WHERE ID_Professor = '".$_SESSION['id']."'";
+            $Salas_Prof_result = mysqli_query($conn, $Salas_Prof_query);
             
             $query_humanas = "SELECT * FROM Humanas WHERE ID_Professor = '".$_SESSION['id']."'";
             $humanas_result = mysqli_query($conn, $query_humanas);
@@ -22,15 +22,15 @@
             $row_exatas = mysqli_num_rows($exatas_result);
             
             if($row_exatas == 1){
-              $query1 = "SELECT * FROM Exatas WHERE ID_Professor = '".$_SESSION['id']."'";
-              $result1 = mysqli_query($conn, $query1);
-              $disciplina = mysqli_fetch_array($result1);
+              $Disciplina_query = "SELECT * FROM Exatas WHERE ID_Professor = '".$_SESSION['id']."'";
+              $Disciplina_result = mysqli_query($conn, $Disciplina_query);
+              $Disciplina = mysqli_fetch_array($Disciplina_result);
             }
 
             if($row_humanas == 1){
-              $query1 = "SELECT * FROM Humanas WHERE ID_Professor = '".$_SESSION['id']."'";
-              $result1 = mysqli_query($conn, $query1);
-              $disciplina = mysqli_fetch_array($result1);
+              $Disciplina_query = "SELECT * FROM Humanas WHERE ID_Professor = '".$_SESSION['id']."'";
+              $Disciplina_result = mysqli_query($conn, $Disciplina_query);
+              $Disciplina = mysqli_fetch_array($Disciplina_result);
             }
 
             echo "<table border='1'>";
@@ -44,14 +44,14 @@
             echo "</thead>";
             echo "<tbody>";
 
-            while($dados = mysqli_fetch_array($result)){
-              $query2 = "SELECT Cadastro.Nome AS Nome, Salas.Sala AS Sala, Notas.Notas AS Notas FROM Verifica_Prova INNER JOIN Cadastro ON Cadastro.ID = Verifica_Prova.ID_Cadastro INNER JOIN Notas ON Notas.ID_Cadastro = Cadastro.ID INNER JOIN Salas ON Salas.ID_Sala = Cadastro.ID_Sala WHERE Verifica_Prova.ID_Sala = '".$dados['ID_Sala']."' AND Verifica_Prova.Disciplina = '".$disciplina['Disciplina']."'";
-              $result2 = mysqli_query($conn, $query2);
-              while($dados2 = mysqli_fetch_array($result2)){
+            while($dados_Salas_Prof = mysqli_fetch_array($Salas_Prof_result)){
+              $Alunos_info_query = "SELECT Cadastro.Nome AS Nome, Salas.Sala AS Sala, Notas.Notas AS Notas FROM Verifica_Prova INNER JOIN Cadastro ON Cadastro.ID = Verifica_Prova.ID_Cadastro INNER JOIN Notas ON Notas.ID_Cadastro = Cadastro.ID INNER JOIN Salas ON Salas.ID_Sala = Cadastro.ID_Sala WHERE Verifica_Prova.ID_Sala = '".$dados_Salas_Prof['ID_Sala']."' AND Verifica_Prova.Disciplina = '".$Disciplina['Disciplina']."'";
+              $Alunos_info_result = mysqli_query($conn, $Alunos_info_query);
+              while($Alunos_info = mysqli_fetch_array($Alunos_info_result)){
                 echo "<tr>";
-                echo "<td>".$dados2['Nome']."</td>";
-                echo "<td>".$dados2['Sala']."</td>";
-                echo "<td>".$dados2['Notas']."</td>";
+                echo "<td>".$Alunos_info['Nome']."</td>";
+                echo "<td>".$Alunos_info['Sala']."</td>";
+                echo "<td>".$Alunos_info['Notas']."</td>";
                 echo "</tr>";
               }
             }
